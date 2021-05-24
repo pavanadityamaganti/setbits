981fb2a031f8960ab3900cc215c44f90c31f06c7
Pavan Aditya <84063768+pavanadityamaganti@users.noreply.github.com>
2021-05-24 15:12:27 +0530

    Update CONTAINsetbitORnot.cpp

--- CONTAINsetbitORnot.cpp
+++ CONTAINsetbitORnot.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 int main()
 {
-	int num;
+	int num,count;
 	cout<<"enter the number :";
 	cin>>num;
-	if((num)&1)
+	while(num!=0)
 	{
-		cout<<"it contain set bit's";
-	}
-    else
-    {
-    	cout<<"it dose'nt contain set bit's";
-	}
+		if((num)&1==1)
+		{
+			count+=1;
+		}
+		else
+		{
+			count=0;
+		}
+		num=num>>1;
+    }
+if(count>0)
+{
+	cout<<"it contain set bit's";
+}
+else
+{
+    cout<<"it dose'nt contain set bit's";
+}
 }
